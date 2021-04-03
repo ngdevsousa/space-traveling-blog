a0df3d95a638a5be119a2b20025edd8d6e325449
f.sousa <ng.dev.sousa@outlook.com>
2021-04-03 18:46:42 -0300

    Add Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="logo.svg" alt="logo" />
+        <p>
+          spacetraveling<span>.</span>
+        </p>
+      </div>
+    </header>
+  );
 }
