64d87bdd55b97436168e96eb8195cfb03db4616f
f.sousa <ng.dev.sousa@outlook.com>
2021-04-06 17:26:24 -0300

    Update Header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a>
-          <img src="logo.svg" alt="logo" />
-          spacetraveling<span>.</span>
-        </a>
+        <Link href="/">
+          <a>
+            <img src="logo.svg" alt="logo" />
+            spacetraveling<span>.</span>
+          </a>
+        </Link>
       </div>
     </header>
   );
